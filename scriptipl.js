2ac83fbd99a11b96d1dc44ce6b6430f7a2263d03
Ojas84 <94777977+Ojas84@users.noreply.github.com>
2021-12-04 18:55:43 +0530

    Add files via upload

---
+++ scriptipl.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAr8p8oLPpDHHcp_EJloWKhCaZzMhoWJFs",
+    authDomain: "sports-website-ec162.firebaseapp.com",
+    databaseURL: "https://sports-website-ec162-default-rtdb.firebaseio.com",
+    projectId: "sports-website-ec162",
+    storageBucket: "sports-website-ec162.appspot.com",
+    messagingSenderId: "705058772948",
+    appId: "1:705058772948:web:d8e31fd60a8a2ed8de2841"
+  };
+  firebase.initializeApp(firebaseConfig);
